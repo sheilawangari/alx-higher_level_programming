0c3b6afac339972753ca1fc20a50085e720858ad
sheilawangari <99507318+sheilawangari@users.noreply.github.com>
2022-07-14 16:34:25 +0300

    Base Class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,15 +1,52 @@
 #!/usr/bin/python3
-""" Base """
+"""base module"""
 
 
 class Base:
-    """Base of all other classes"""
+    """
+    Base class from which all other classes will be derived.
+    ...
+    Attributes
+    ----------
+    __nb_objects : int
+        global variable for number of instances
+    Methods
+    -------
+    to_json_string(list_dictionaries)
+        returns JSON string representation of list_dictionaries, \
+                returns "[]" if list_dictionaries is None
+    save_to_file(cls, list_objs)
+        writes the JSON string representation of list_objs to a file
+    """
+
+    global __nb_objects
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initializes a Base object"""
+        """
+        class constructor
+        """
+        global __nb_objects
         if id is not None:
             self.id = id
         else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+            __nb_objects += 1
+            self.id = __nb_objects
+
+    def to_json_string(list_dictionaries):
+        """
+        returns the JSON string representation of list_dictionaries
+        """
+        import json
+
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
+    def save_to_file(cls, list_objs):
+        """
+        writes the JSON string representation of list_objs to a file
+        """
+        pass
+    
