20176d38f85a050ba398b24677076dc466f4b5f5
sheilawangari <99507318+sheilawangari@users.noreply.github.com>
2022-10-26 23:03:42 +0300

    Write a JavaScript script that fetches from https://fourtonfish.com/hellosalut/?lang=fr and displays the value of hello from that fetch in the HTML tag DIV#hello

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    const url = 'https://fourtonfish.com/hellosalut/?lang=fr';
+    $.get(url, function (data) {
+      $('DIV#hello').text(data.hello);
+    });
+  };
+  
